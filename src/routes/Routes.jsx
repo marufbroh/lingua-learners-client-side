4fb03cbb7a0b3caaba28197cabcdc9abb4a6ca17
marufbroh <marufmd775@gmail.com>
2023-06-08 12:18:37 +0600

    basic router setup complete

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Account/Login";
+import Register from "../pages/Account/Register";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/login",
+                element: <Login />
+            },
+            {
+                path: "/register",
+                element: <Register />
+            },
+        ]
+    },
+]);
+
+export default router
