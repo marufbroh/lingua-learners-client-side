a55b5809d21b0b0836fa569082fad6841d1169e4
marufbroh <marufmd775@gmail.com>
2023-06-09 15:42:12 +0600

    installed react query and implemented on admin

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,21 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes.jsx'
 import AuthProvider from './providers/AuthProvider.jsx'
 import { Toaster } from 'react-hot-toast';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
-      <Toaster />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <Toaster />
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>,
 )
