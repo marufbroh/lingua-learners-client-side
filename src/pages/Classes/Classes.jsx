a8779b1fb237657a8cfb7cdcd03995b9d2ce089d
marufbroh <marufmd775@gmail.com>
2023-06-10 11:10:03 +0600

    classes page and instructor page data shown done

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import useClasses from '../../hooks/useClasses';
+import ClassCard from '../Home/ClassCard';
 
 const Classes = () => {
+    const [classes] = useClasses();
+    const approvedClasses = classes.filter(classItem => classItem.status === "approved");
+    const sortedClasses = approvedClasses.sort((a, b) => b.enrolled_students - a.enrolled_students);
+    // console.log(sortedClasses)
     return (
-        <div>
-            Classes
+        <div className='container mx-auto'>
+            <h3 className='font-bold text-3xl'>Top Enrolled Classes</h3>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-6'>
+                {
+                    sortedClasses.map(classItem => <ClassCard key={classItem._id} classItem={classItem} />)
+                }
+            </div>
         </div>
     );
 };
