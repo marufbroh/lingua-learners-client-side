d605d5e21937c67ca34e679a150426613e4237e7
marufbroh <marufmd775@gmail.com>
2023-06-10 22:41:37 +0600

    selected classes page data shown done

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -20,9 +20,11 @@ const Dashboard = () => {
         <div>
             <div className="drawer lg:drawer-open">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col items-center justify-center">
+                <div className="drawer-content">
                     {/* Page content here */}
-                    <label htmlFor="my-drawer-2" className="btn btn-outline drawer-button lg:hidden">Open drawer</label>
+                    <div className='text-center'>
+                        <label htmlFor="my-drawer-2" className="btn btn-outline drawer-button lg:hidden">Open drawer</label>
+                    </div>
                     <Outlet />
 
                 </div>
--- src/pages/Dashboard/UserDashboard/MySelectedClasses.jsx
+++ src/pages/Dashboard/UserDashboard/MySelectedClasses.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import useSelectedClassess from '../../../hooks/useSelectedClassess';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const MySelectedClasses = () => {
+    const [selectedClasses, refetch] = useSelectedClassess();
+    // console.log(selectedClasses)
+
+    const handleDelete = () => {
+        //tttt
+    }
+
+    const handlePayment = () => {
+        //
+    }
     return (
-        <div>
-            sellectf classes
+        <div className='container lg:w-3/4 mx-auto'>
+            <h3 className='text-3xl font-bold'>Your all selected classes</h3>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Class Image</th>
+                            <th>Class Name</th>
+                            <th>Instructor Name</th>
+                            <th>Price</th>
+                            <th>Delete</th>
+                            <th>Pay</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            selectedClasses.map((selectedClass, index) => <tr key={selectedClass._id}>
+                                <td>{index + 1}</td>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={selectedClass.class_image} alt="" />
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>{selectedClass.class_name}</td>
+                                <td>{selectedClass.instructor_name}</td>
+                                <td>{selectedClass.price}</td>
+                                <td>
+                                    <button onClick={() => handleDelete(selectedClass)} className="btn btn-error text-lg"><FaTrashAlt /></button>
+                                </td>
+                                <td>
+                                    <button onClick={() => handlePayment(selectedClass)} className="btn btn-success">Pay</button>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
